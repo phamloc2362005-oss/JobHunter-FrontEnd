50692f7c3d41cfec941787417180875e186270c5
phamloc2362005 <phamloc2362005@gmail.com>
2026-05-12 18:27:28 +0700

    update

--- src/pages/company/index.tsx
+++ src/pages/company/index.tsx
@@ -1,7 +1,9 @@
 import { Col, Row } from 'antd';
+import { CodeOutlined } from '@ant-design/icons';
 import styles from 'styles/client.module.scss';
 import CompanyCard from '@/components/client/card/company.card';
 import s from './index.module.scss';
+import homeStyles from '@/pages/home/index.module.scss';
 
 const ClientCompanyPage = (props: any) => {
     return (
@@ -50,6 +52,51 @@ const ClientCompanyPage = (props: any) => {
                     </Row>
                 </div>
             </section>
+
+            {/* ══════════════════ FOOTER ══════════════════ */}
+            <footer className={homeStyles.footer}>
+                <div className={`${styles['container']} ${homeStyles.footerInner}`}>
+                    <div className={homeStyles.footerBrand}>
+                        <div className={homeStyles.footerLogo}>
+                            <CodeOutlined />
+                        </div>
+                        <strong>JobHunter</strong>
+                        <p>Platform tuyển dụng IT hàng đầu Việt Nam. Built for developers, by developers.</p>
+                    </div>
+
+                    <div className={homeStyles.footerLinks}>
+                        <div className={homeStyles.footerCol}>
+                            <h4>Platform</h4>
+                            <a href="/">Trang Chủ</a>
+                            <a href="/job">Việc Làm</a>
+                            <a href="/company">Công Ty</a>
+                            <a href="/skills">Kỹ Năng</a>
+                        </div>
+                        <div className={homeStyles.footerCol}>
+                            <h4>Nhà Tuyển Dụng</h4>
+                            <a href="#">Đăng tin tuyển dụng</a>
+                            <a href="#">Tìm ứng viên</a>
+                            <a href="#">Dashboard công ty</a>
+                            <a href="#">Bảng giá</a>
+                        </div>
+                        <div className={homeStyles.footerCol}>
+                            <h4>Liên Hệ</h4>
+                            <a href="mailto:support@jobhunter.com">support@jobhunter.com</a>
+                            <a href="tel:+84123456789">+84 123 456 789</a>
+                            <span>TP. Hồ Chí Minh, Việt Nam</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={homeStyles.footerBottom}>
+                    <div className={styles['container']}>
+                        <span>© 2024 JobHunter. All rights reserved.</span>
+                        <div className={homeStyles.footerBottomLinks}>
+                            <a href="#">Privacy Policy</a>
+                            <a href="#">Terms of Service</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     )
 }
